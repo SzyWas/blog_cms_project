80ab20c62dc141d1870bbe976f38933d298be866
SzWas <sz.wasiewski@gmail.com>
2016-10-14 01:02:02 +0200

    Added: CMS for Comments

--- cms/urls.py
+++ cms/urls.py
@@ -1,29 +1,40 @@
 from django.conf.urls import url
 from blog.models import PostAuthor, PostEntry
 from category.models import Category
-
+from comment.models import Comment
 from blog.views import PostEntryContentManagementView, PostContentManagementView, \
     PostEntryCreateView, PostEntryEditView, PostAuthorContentManagementView
 from cms.views import  ObjectCreateView, ObjectEditView, ContentManagementView, ObjectDeleteView
 from category.views import CategoryContentManagementView
+from comment.views import ApprovedCommentView, CommentEditView
+
 
 urlpatterns = [
 
     url(r'^$', ContentManagementView.as_view(), name='cms'),
+
+    url(r'^authors-list/', PostAuthorContentManagementView.as_view(model=PostAuthor, context_object_name='category'),
+        name='authors-list'),
+    url(r'^author-create/', ObjectCreateView.as_view(model=PostAuthor), name='author-create'),
+    url(r'^author-edit/(?P<slug>[\w-]+)/(?P<pk>[0-9]+)/', ObjectEditView.as_view(model=PostAuthor), name='author-edit'),
+    url(r'^author-delete/(?P<pk>[0-9]+)/', ObjectDeleteView.as_view(model=PostAuthor), name='author-delete'),
+
+    url(r'^category-list/', CategoryContentManagementView.as_view(model=Category, context_object_name='category'),
+        name='category-list'),
+    url(r'^category-create/', ObjectCreateView.as_view(model=Category), name='category-create'),
+    url(r'^category-edit/(?P<slug>[\w-]+)/(?P<pk>[0-9]+)/', ObjectEditView.as_view(model=Category),
+        name='category-edit'),
+    url(r'^category-delete/(?P<pk>[0-9]+)/', ObjectDeleteView.as_view(model=Category), name='category-delete'),
+
+    url(r'^comment-approved/(?P<pk>[0-9]+)/', ApprovedCommentView.as_view(), name='comment-approved'),
+    url(r'^comment-edit/(?P<pk>[0-9]+)/', CommentEditView.as_view(), name='comment-edit'),
+    url(r'^comment-delete/(?P<pk>[0-9]+)/', ObjectDeleteView.as_view(model=Comment), name='comment-delete'),
+
     url(r'^post-list/', PostContentManagementView.as_view(), name='post-list'),
     url(r'^post/(?P<slug>[\w-]+)/(?P<pk>[0-9]+)/', PostEntryContentManagementView.as_view(), name='post-cms'),
     url(r'^post-create/', PostEntryCreateView.as_view(), name='post-create'),
     url(r'^post-edit/(?P<slug>[\w-]+)/(?P<pk>[0-9]+)/', PostEntryEditView.as_view(), name='post-edit'),
     url(r'^post-delete/(?P<pk>[0-9]+)/', ObjectDeleteView.as_view(model=PostEntry), name='post-delete'),
 
-    url(r'^category-list/', CategoryContentManagementView.as_view(model=Category, context_object_name='category'), name='category-list'),
-    url(r'^category-create/', ObjectCreateView.as_view(model=Category), name='category-create'),
-    url(r'^category-edit/(?P<slug>[\w-]+)/(?P<pk>[0-9]+)/', ObjectEditView.as_view(model=Category), name='category-edit'),
-    url(r'^category-delete/(?P<pk>[0-9]+)/', ObjectDeleteView.as_view(model=Category), name='category-delete'),
-
-    url(r'^authors-list/', PostAuthorContentManagementView.as_view(model=PostAuthor, context_object_name='category'), name='authors-list'),
-    url(r'^author-create/', ObjectCreateView.as_view(model=PostAuthor), name='author-create'),
-    url(r'^author-edit/(?P<slug>[\w-]+)/(?P<pk>[0-9]+)/', ObjectEditView.as_view(model=PostAuthor), name='author-edit'),
-    url(r'^author-delete/(?P<pk>[0-9]+)/', ObjectDeleteView.as_view(model=PostAuthor), name='author-delete'),
 
 ]
--- comment/views.py
+++ comment/views.py
@@ -1,5 +1,24 @@
 from comment.models import Comment
 from blog.models import PostEntry
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, TemplateView
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+
+
+class ApprovedCommentView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    fields = [
+        'approved',
+    ]
+    success_url = reverse_lazy('cms')
+    template_name = 'cms/object/object_edit.html'
+
+
+class CommentEditView(ApprovedCommentView):
+    fields = [
+        'approved',
+        'content',
+        'author',
+        'email',
+    ]
